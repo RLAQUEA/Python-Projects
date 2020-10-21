d17c3a48a91be2f9aab9f7e2ecc99ce9f1d474cf
RLAQUEA <30808137+RLAQUEA@users.noreply.github.com>
2020-10-20 20:00:31 -0400

    Add files via upload

---
+++ Age_calculator.py
@@ -0,0 +1,10 @@
+
+def calculate_age(current_year, birth_year):
+  age = current_year - birth_year
+  return age
+
+my_age = calculate_age(current_year = 2049, birth_year = 1993 )
+
+
+dads_age = calculate_age(current_year = 2049, birth_year = 1953)
+

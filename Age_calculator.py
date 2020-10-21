80069eb28193f33829e03191413bed76ac86eecc
RLAQUEA <30808137+RLAQUEA@users.noreply.github.com>
2020-10-21 13:35:28 -0400

    Update Age_calculator.py

--- Age_calculator.py
+++ Age_calculator.py
@@ -8,3 +8,18 @@ my_age = calculate_age(current_year = 2049, birth_year = 1993 )
 
 dads_age = calculate_age(current_year = 2049, birth_year = 1953)
 
+
+
+
+
+
+"""current_year = 2048
+def calculate_age(birth_year):
+  age = current_year - birth_year
+  return age
+
+print(current_year)
+
+print(calculate_age(1970))"""
+
+
